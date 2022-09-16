9e7a1dc3f14bb4578a2673f502235f1fafb3e259
Ziqing Yan <80880595+ZiqingY@users.noreply.github.com>
2022-09-16 22:41:20 +0100

    Update 11.except.py

--- 11.except.py
+++ 11.except.py
@@ -8,7 +8,7 @@ except:
 '''例2'''
 try:
     print(21/0)
-    print(123+"shit")
+    print(123+"hello")
 except ArithmeticError as e:  # 专业的数学报错解释
     print(e)
 
@@ -16,7 +16,7 @@ except ArithmeticError as e:  # 专业的数学报错解释
 '''例3'''
 try:
     print(21/1)
-    print(123+"shit")
+    print(123+"hello")
 except ArithmeticError as e:  # 专业的数学报错解释
     print(e)
 except TypeError as a:        # 专业的语法报错解释
@@ -26,6 +26,6 @@ except TypeError as a:        # 专业的语法报错解释
 '''例4'''
 try:
     print(21/1)
-    print(123+"shit")
+    print(123+"hello")
 except Exception:              # 自定义报错的内容
-    print("you fucked it up")
+    print("you messed it up")
