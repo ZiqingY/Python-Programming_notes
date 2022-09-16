ca76d7ffe064eb8b1e45f4ff68b0bb39cf0e1c46
Ziqing Yan <80880595+ZiqingY@users.noreply.github.com>
2022-09-16 21:37:47 +0100

    Update 2.int float list.py

--- 2.int float list.py
+++ 2.int float list.py
@@ -1,29 +1,29 @@
-'''数据类型int'''
+'''data type: int'''
 a=10
 print(type(a))
 
 
-'''数据类型float'''
+'''float'''
 b=10.5
 print(type(b))
 
 
-'''数据类型string'''
+'''string'''
 name="zacyanneverquit"
 print(name[2])
 print(len(name))
-print(name[2:5])          # 第三个到第五个
+print(name[2:5])
 print(name*2)
 print(name+" is true")
 
 
-'''数据类型list'''
-mylist=["啦啦啦", 9, "English", 2020, 2020]
+'''list'''
+mylist=["你好吗", 9, "English", 2020, 2020]
 print(type(mylist))
 print(len(mylist))
 print(mylist[0])
-mylist.append(5000)        # 增加元素
+mylist.append(5000)        # adding elements
 print(mylist)
-mylist.remove(5000)        # 删除元素
+mylist.remove(5000)        # delete elements
 print(mylist)
-print(mylist.count(2020))  # 统计某元素的个数
+print(mylist.count(2020))  # counting elements
